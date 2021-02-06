74feee324bca19e3170f5f8677f96d90f59690e8
Rohald van Merode <rohald_89@live.nl>
2021-02-06 21:55:03 +0100

    added change of opacity to pricing labels when toggle changes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const basicPlan = document.querySelector('#price-basic');
 const proPlan = document.querySelector('#price-pro');
 const businessPlan = document.querySelector('#price-business');
 const periods = document.querySelectorAll('.period');
+const monthly = document.querySelector('#monthly');
+const yearly = document.querySelector('#yearly');
 
 menu.addEventListener('click', () => {
     header.classList.toggle('active');
@@ -23,6 +25,8 @@ switchLabel.addEventListener('click', () => {
         periods.forEach(period => {
             period.innerText = 'per year';
         });
+        yearly.style.opacity = '1';
+        monthly.style.opacity = '0.5';
     } else {
         basicPlan.innerText = '$19.00';
         proPlan.innerText = '$39.00';
@@ -30,5 +34,7 @@ switchLabel.addEventListener('click', () => {
         periods.forEach(period => {
             period.innerText = 'per month';
         });
+        yearly.style.opacity = '0.5';
+        monthly.style.opacity = '1';
     }
 })
