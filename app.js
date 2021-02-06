836d78e68c50ccb2a8c6514474afdfdc0f6f46d0
Rohald van Merode <rohald_89@live.nl>
2021-02-06 21:13:37 +0100

    improved mobile navigation and added JS for pricing toggle and mobile menu

---
+++ app.js
@@ -0,0 +1,34 @@
+const menu = document.querySelector('.menu');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const switchLabel = document.querySelector('.switch');
+const toggle = document.querySelector('#toggle');
+const basicPlan = document.querySelector('#price-basic');
+const proPlan = document.querySelector('#price-pro');
+const businessPlan = document.querySelector('#price-business');
+const periods = document.querySelectorAll('.period');
+
+menu.addEventListener('click', () => {
+    header.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
+
+
+switchLabel.addEventListener('click', () => {
+    console.log(toggle)
+    if (toggle.checked === true){
+        basicPlan.innerText = '$190.00';
+        proPlan.innerText = '$390.00';
+        businessPlan.innerText = '$990.00';
+        periods.forEach(period => {
+            period.innerText = 'per year';
+        });
+    } else {
+        basicPlan.innerText = '$19.00';
+        proPlan.innerText = '$39.00';
+        businessPlan.innerText = '$99.00';
+        periods.forEach(period => {
+            period.innerText = 'per month';
+        });
+    }
+})
